7f1858ffd17c9fbc984f112ad634c2b4de0d20b7
kripa01 <69767619+kripa01@users.noreply.github.com>
2024-10-30 15:48:12 -0400

    First draft
    The base layout is done, still need to fix the styling and add the rest of the milestone information

--- technolgap/src/env.d.ts
+++ technolgap/src/env.d.ts
@@ -2,6 +2,7 @@
 
 interface ImportMetaEnv {
     readonly CONTENTFUL_SPACE_ID: string;
+    readonly CONTENTFUL_ACCESS_TOKEN: string; 
     readonly CONTENTFUL_DELIVERY_TOKEN: string;
     readonly CONTENTFUL_PREVIEW_TOKEN: string;
   }
